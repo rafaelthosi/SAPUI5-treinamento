7809dea2dd638d130e6b69670061165a195ddb8f
rafaelthosi <thhrafael@outlook.com>
2020-11-26 16:32:33 +0000

    Adicionando campo de busca
    Change-Id: I613ccc2d20d89083a24944e4daccb1687819bed6

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -3,11 +3,31 @@ sap.ui.define([
 	"sap/m/MessageToast",
 	"sap/ui/model/json/JSONModel",
 	"sap/ui/model/resource/ResourceModel",
-	"../formatter"
-], function (Controller, MessageToast, JSONModel, ResourceModel, formatter) {
+	"../formatter",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+	
+], function (Controller, MessageToast, JSONModel, ResourceModel, formatter, Filter, FilterOperator) {
 	"use strict";
 	return Controller.extend("udemy.openui5.passoapasso.controller.App", {
 		formatter: formatter,
+		
+		onFilterInvoices : function (oEvent) {
+		
+			// build filter array
+			var aFilter = [];
+			var sQuery = oEvent.getParameter("query");
+			
+			if (sQuery) {
+				aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+			}
+			
+			// filter binding
+			var oList = this.byId("invoiceList");
+			var oBinding = oList.getBinding("items");
+			oBinding.filter(aFilter);
+		},
+
 		onInit: function () {
 			// set data model on view
 			var oData = {
@@ -27,6 +47,8 @@ sap.ui.define([
 			});
 			this.getView().setModel(i18nModel, "i18n");
 		},
+	
+	
 
 		onShowHello: function () {
 
